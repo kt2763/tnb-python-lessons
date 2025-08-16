b8450502ec4251bdabb0df940bcaa29fa37c8770
kt2763 <k.eugene.t@gmail.com>
2025-08-16 13:44:55 +0900

    lesson 3 f-string

---
+++ 3-fstring/lesson3_1.py
@@ -0,0 +1,5 @@
+print("Hello\nWorld")  # 改行
+print("A\tB\tC")  # タブ
+print("C:\\Users\\Name")  # パス
+print('He said, "Python!"')
+# こっちもいける print("He said, \"Python!\"")
---
+++ 3-fstring/lesson3_3.py
@@ -0,0 +1,3 @@
+print("{}は{}歳です".format("Tanabe", 36))
+print("{0}は{1}歳です。{0}はPythonが好きです".format("Tanabe", 36))
+print("{name}は{age}歳です".format(name="Tanabe", age=36))
